724e44eb0ce262977389ae3033d3cd979ad3130a
corentinvrmln <corentin.vermeulen@student.uclouvain.be>
2024-03-15 17:56:21 +0100

    refactoring

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ n_obs = env.observation_space.shape[0]
 n_actions = env.action_space.n
 
 hparams = {"layer_sizes": [256, 256, 256, 256],
-           "EPOCHS": 1000,
+           "EPOCHS": 300,
            "BATCH_SIZE": 256,
            "EPS_DECAY": 2000}
 
 # Easy game
-agent = DQNAgent_simple(env, hparams)
+agent = DQNAgent_simple(env, hparams, root='TEST')
 agent.update_env({"PLAYER_FLAP_ACC": -6, "PLAYER_ACC_Y": 1, "pipes_are_random":True})
 agent.train(name="test")
 
