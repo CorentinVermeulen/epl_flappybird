b335c9cfd7b56bc686fbeb7f0aa34c65882e28a5
corentinvrmln <corentin.vermeulen@student.uclouvain.be>
2024-03-18 11:33:28 +0100

    ceci: run script

--- agents/DQN_agent_ceci.py
+++ agents/DQN_agent_ceci.py
@@ -75,8 +75,7 @@ class DQN(nn.Module):
 
 class DQNAgent_simple_cuda():
     def __init__(self, env, hyperparameters, root_path= "runs/default/"):
-        #self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.device='mps'
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.env = env
         self.n_actions = env.action_space.n  # Get number of actions from gym action space
         self.n_observations = len(env.reset())  # Get the number of state observations
