8c006f80dd934b95f218441664c90ece5e4c3545
corentinvrmln <corentin.vermeulen@student.uclouvain.be>
2024-04-01 23:15:54 +0200

    refactoring

--- source/exp_2.py
+++ source/exp_2.py
@@ -14,17 +14,17 @@ baseline_HP = {"EPOCHS": 750,
                "EPS_DECAY": 2500,
                "TAU": 0.01,
                "LAYER_SIZES": [256, 256, 256, 256],
-               "GAMMA": 0.99,
+               "GAMMA": 0.999,
                "UPDATE_TARGETNET_RATE": 3,
                "BATCH_SIZE": 256,
-               "LR": 1e-4,
+               "LR": 1e-5,
                }
 
 """ EXP 2: UPDATE RATE ?
 LAYER SIZE: [256, 256, 256, 256] or [64, 128, 256, 512, 256, 128] ? [256, 256, 256, 256] slightly better but is faster
 GAMMA: 0.99 or 0.999 ? 0.99 is better
 UPDATE_TARGETNET_RATE: 1 - 3 - 5
-LR: 1e-3 - 1e-4 - 1e-5 ? 
+LR: 1e-3 - 1e-4 - 1e-5 ? 1e-5 is better
 """
 
 ## ENVIRONMENT CONTEXT
@@ -32,22 +32,22 @@ game_context = {'PLAYER_FLAP_ACC': -5, 'PLAYER_ACC_Y': 1, 'pipes_are_random': Fa
 
 ## LEARNING PARAMETERS
 root = '../../experiments/update_rate/'
-update_rate = [1,3,5]
+param = [1,3,5]
 iters = 5
 
 print(f"Python script root: {os.getcwd()}")
-print(f"Starting {len(update_rate)*iters} experiments at {root}")
+print(f"Starting {len(param)*iters} experiments at {root}")
 print("Device cuda? ", torch.cuda.is_available())
 
-for j in range(len(update_rate)):
+for j in range(len(param)):
     current_hp = baseline_HP.copy()
-    current_hp.update({"UPDATE_TARGETNET_RATE": update_rate[j]})
+    current_hp.update({"UPDATE_TARGETNET_RATE": param[j]})
     for rep in range(iters):
         t = time.perf_counter()
         env = FlappyBirdEnv()
         agent = AgentSimple(FlappyBirdEnv(), HParams(current_hp), root_path=root)
         agent.update_env(game_context)
-        scores, durations = agent.train(show_progress=False, name=f'{j}{rep}')
+        scores, durations = agent.train(show_progress=False, name=f'{j+1}{rep+1}')
         HS = np.max(scores)
         MD = np.mean(durations)
         MD_last = np.mean(durations[-250:])
