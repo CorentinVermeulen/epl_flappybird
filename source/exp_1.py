dc67ed75fab649b0660ea58f769fe6de3619a196
corentinvrmln <corentin.vermeulen@student.uclouvain.be>
2024-04-02 10:41:30 +0200

    gridsearch

--- source/exp_1.py
+++ source/exp_1.py
@@ -12,49 +12,61 @@ baseline_HP = {"EPOCHS": 750,
                "EPS_START": 0.9,
                "EPS_END": 0.001,
                "EPS_DECAY": 2500,
-               "TAU": 0.01,
                "LAYER_SIZES": [256, 256, 256, 256],
-               "GAMMA": 0.99,
+               "GAMMA": 0.999,
                "UPDATE_TARGETNET_RATE": 3,
                "BATCH_SIZE": 256,
-               "LR": 1e-4,
+               "LR": 1e-5,
+               "TAU": 0.01,
                }
 """ EXP 1 : GAMMA ? 
 LAYER SIZE: [256, 256, 256, 256] or [64, 128, 256, 512, 256, 128] ? [256, 256, 256, 256] slightly better but is faster
-GAMMA: 0.99 or 0.999 ? 0.99 is better
+GAMMA: 0.99 or 0.999 ? 0.999 is better
 UPDATE_TARGETNET_RATE: 1 - 3 - 5
-LR: 1e-3 - 1e-4 - 1e-5 ? 
+LR: 1e-3 - 1e-4 - 1e-5 ? 1e-5 is better
 """
 
 ## ENVIRONMENT CONTEXT
 game_context = {'PLAYER_FLAP_ACC': -5, 'PLAYER_ACC_Y': 1, 'pipes_are_random': False}
 
 ## LEARNING PARAMETERS
-root = '../../experiments/lrs/'
-values = [1e-3, 1e-4, 1e-5]
-iters = 5
+root = '../../experiments/gridsearch/'
+
+iters = 3
+gammas = [0.99, 0.999]
+lrs = [1e-4, 1e-5]
+update_rates = [1, 3, 5]
+taus = [0.01, 0.001]
+n = len(gammas) * len(lrs) * len(update_rates) * len(taus)
 
 print(f"Python script root: {os.getcwd()}")
-print(f"Starting {len(values)*iters} experiments at {root}")
+print(f"Starting {n*iters} experiments at {root}")
 print("Device cuda? ", torch.cuda.is_available())
 
-for j in range(len(values)):
-    current_hp = baseline_HP.copy()
-    current_hp.update({"LR": values[j]})
-    for rep in range(iters):
-        t = time.perf_counter()
-        env = FlappyBirdEnv()
-        agent = AgentSimple(FlappyBirdEnv(), HParams(current_hp), root_path=root)
-        agent.update_env(game_context)
-        scores, durations = agent.train(show_progress=False, name=f'{j}{rep}')
-        HS = np.max(scores)
-        MD = np.mean(durations)
-        MD_last = np.mean(durations[-250:])
-        te = time.perf_counter() - t
-        print(
-            f"{j+1} {rep+1}\n"
-            f"\tS* {HS:<4.0f} - E[D] {MD:<5.0f} - E[D]_250 {MD_last:<5.0f} "
-            f"- Time {int(te // 60):02}:{int(te % 60):02}"
-        )
+for tau in taus:
+    for gamma in gammas:
+        for lr in lrs:
+            for u_rate in update_rates:
+                current_hp = baseline_HP.copy()
+                current_hp.update({"TAU": tau,
+                                   "GAMMA": gamma,
+                                   "LR": lr,
+                                   "UPDATE_TARGETNET_RATE": u_rate, })
+                for rep in range(iters):
+                    t = time.perf_counter()
+                    env = FlappyBirdEnv()
+                    agent = AgentSimple(FlappyBirdEnv(), HParams(current_hp), root_path=root)
+                    agent.update_env(game_context)
+                    scores, durations = agent.train(show_progress=False, name=f'T{tau}_G{gamma}_LR{lr}_UR{u_rate}_R{rep}')
+                    HS = np.max(scores)
+                    MD = np.mean(durations)
+                    MD_last = np.mean(durations[-250:])
+                    te = time.perf_counter() - t
+                    print(
+                        f"T{tau}_G{gamma}_LR{lr}_UR{u_rate}_R{rep}\n"
+                        f"\tS* {HS:<4.0f} - E[D] {MD:<5.0f} - E[D]_250 {MD_last:<5.0f} "
+                        f"- Time {int(te // 60):02}:{int(te % 60):02}"
+                    )
+
 
 #make_experiment_plot(root)
