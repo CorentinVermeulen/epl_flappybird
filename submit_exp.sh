281c6d76b74b4f33d84c97e185626753de01b391
corentinvrmln <corentin.vermeulen@student.uclouvain.be>
2024-05-20 11:55:31 +0200

    refactoring

--- submit_exp.sh
+++ submit_exp.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+#SBATCH --job-name= exp_multi_job
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=5
+#SBATCH --time=05-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH -J exp_multi
 
 # Chargement des modules
-# module load python/3.x
+module load CUDA
 
 # Exécution script Python
 python exp_multi.py
